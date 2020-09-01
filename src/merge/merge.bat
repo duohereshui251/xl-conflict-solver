95d29d85b97c1577994d75931699ef1d1467b1c0
duohereshui251 <10086@qq.com>
2020-09-01 23:50:43 +0800

    修改merge脚本

--- src/merge/merge.bat
+++ src/merge/merge.bat
@@ -5,7 +5,7 @@ echo "# Merge driver called #"
 cp "%2" temp_a.xlsx
 cp "%3" temp_b.xlsx
 python.exe src/merge/merge.py temp_a.xlsx temp_b.xlsx
-sleep 3
-cp temp_a.xlsx test/c.xlsx
+rm test/a.xlsx
+cp temp_a.xlsx test/a.xlsx
 rm temp_a.xlsx temp_b.xlsx
 exit 0
