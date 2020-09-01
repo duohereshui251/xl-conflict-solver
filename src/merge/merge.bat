353d4589ed4872aebed57040aae4716cc4b441a5
duohereshui251 <10086@qq.com>
2020-09-01 22:58:02 +0800

    www

--- src/merge/merge.bat
+++ src/merge/merge.bat
@@ -1,6 +1,7 @@
 
 echo "# Merge driver called #"
 
-"C:\Program Files\Microsoft Office\root\Office16\EXCEL.exe" "%2" "/batOpen"
 
+"C:\Program Files\Microsoft Office\root\Office16\EXCEL.exe" "%2" 
+sleep 6
 exit 0
