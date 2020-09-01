4952a04a0722ef94570de49fe1d91e58bdc6ed42
unknown <Administrator@sh-nongyanhua.SNDA.ROOT.CORP>
2020-09-01 16:57:35 +0800

    修改merge脚本

---
+++ src/merge/merge.bat
@@ -0,0 +1,36 @@
+echo ""
+echo "#######################"
+echo "# Merge driver called #"
+echo "#######################"
+echo ""
+
+echo "ancestor: $1"
+echo "---------"
+cat $1
+echo ""
+
+echo " current: $2"
+echo "---------"
+cat $2
+echo ""
+
+echo "   other: $3"
+echo "---------"
+cat $3
+echo ""
+
+echo "Resolving conflict ..."
+
+echo "This is the merge result" > $2
+echo "Conflict resolved!"
+
+echo "resolved: $2"
+echo "---------"
+cat $2
+echo ""
+
+echo "try $4"
+cat $4
+REM python.exe src/merge/merge.py 
+
+exit 0
