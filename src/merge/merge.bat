fff47e363116242ed4e8e1d19ec553d11f07b8d3
duohereshui251 <10086@qq.com>
2020-09-02 00:26:08 +0800

    修改merge脚本

--- src/merge/merge.bat
+++ src/merge/merge.bat
@@ -6,7 +6,7 @@ cp "%2" temp_a.xlsx
 cp "%3" temp_b.xlsx
 rm "%1"
 python.exe src/merge/merge.py temp_a.xlsx temp_b.xlsx
-rm test/a.xlsx
-cp temp_a.xlsx test/a.xlsx
+
+cat temp_a.xlsx > test/a.xlsx
 rm temp_a.xlsx temp_b.xlsx
 exit 0
