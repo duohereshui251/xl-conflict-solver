fbdf81f98c49353b99f76bc5935493dc29ddc5f1
unknown <Administrator@sh-nongyanhua.SNDA.ROOT.CORP>
2020-09-01 15:11:06 +0800

    修改diff

--- src/merge.py
+++
@@ -1,7 +0,0 @@
-from src import diff
-
-if __name__ == '__main__':
-    
-
-
-
---
+++ src/merge/merge.py
@@ -0,0 +1,38 @@
+from Diff.diff import make_diff
+import sys, os
+import xlwings as xw
+
+def make_merge(args = None):
+    if not args: args = sys.argv
+
+    if not 8 <= len(args) <= 9:
+        print('Unexpected number of arguments: {0}'.format(len(args)))
+        sys.exit(0)
+    # 参数为8个
+    if len(args) == 8:
+        _, workbook_name, workbook_b, _, _, workbook_a, _ , _ = args
+        numlines = 3
+
+    # 参数为9个
+    if len(args) == 9:
+        _, numlines, workbook_name, workbook_b, _, _, workbook_a, _, _ = args
+        numlines = int(numlines)
+
+    diffs = make_diff()
+
+    book_a_path = os.path.abspath(workbook_a) if workbook_a != 'nul' and workbook_a != '/dev/null' else None
+    book_b_path = os.path.abspath(workbook_b) if workbook_b != 'nul' and workbook_b != '/dev/null' else None
+    book_a = xw.Book(book_a_path) if book_a_path else None
+    book_b = xw.Book(book_b_path) if book_b_path else None
+
+    sheets = []
+    for sht in book_a.sheets:
+        sheets.append(sht.name)
+    pass
+
+if __name__ == '__main__':
+    print(sys.argv)
+    # make_merge()
+
+
+
