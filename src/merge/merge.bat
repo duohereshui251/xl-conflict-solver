621d5e93015f9ab76c5f0d72bf4b48cb239b71bc
duohereshui251 <10086@qq.com>
2020-09-01 23:02:10 +0800

    测试暂存文件

--- src/merge/merge.bat
+++ src/merge/merge.bat
@@ -2,7 +2,9 @@
 echo "# Merge driver called #"
 
 
-cp "%2" temp.xlsx
-"C:\Program Files\Microsoft Office\root\Office16\EXCEL.exe" "%2" 
-sleep 6
+cp "%2" temp_a.xlsx
+cp "%3" temp_b.xlsx
+"C:\Program Files\Microsoft Office\root\Office16\EXCEL.exe" temp_a.xlsx
+"C:\Program Files\Microsoft Office\root\Office16\EXCEL.exe" temp_b.xlsx
+
 exit 0
