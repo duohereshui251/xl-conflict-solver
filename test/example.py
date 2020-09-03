2a68e6f456ddb2c19a8a391e4bbd2c08bc7c1fda
hh <10086@qq.com>
2020-09-03 15:19:12 +0800

    修改example.py

--- test/example.py
+++ test/example.py
@@ -6,7 +6,8 @@ s = sys.argv[1]
 print('write {} to a.xlsx in cell (1,2)'.format(s))
 
 book_a = xw.Book('test/a.xlsx')
-book_a.sheets[0].range((1,2)).value = s
+book_a.sheets[0].range((1, 2)).value = s
 book_a.save()
-pid = xw.apps.keys()[0]
-xw.apps[pid].kill()
+keys = xw.apps.keys()
+for key in keys:
+    xw.apps[key].kill()
