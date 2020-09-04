65807c9cc7b718c44b376d7f92b7baabb435b690
hh <10086@qq.com>
2020-09-04 10:59:15 +0800

    修改测试脚本的错误

--- test/example.py
+++ test/example.py
@@ -2,11 +2,13 @@ import xlwings as xw
 import sys
 
 s = sys.argv[1]
-change_f = sys.argv[2]
+if len(sys.argv > 3):
+    change_f = sys.argv[2]
 
 book_a = xw.Book('test/a.xlsx')
 book_a.sheets[0].range((1, 2)).value = s
-if change_f.split():
+
+if len(sys.argv > 3) and change_f.split():
     book_a.sheets[0].range(1,3).formula = '=SUM(A1:B1)+{}'.format(s)
     
 book_a.save()
