93a88adef5ee657bd964a2308da25068acb350a8
hh <10086@qq.com>
2020-09-04 10:56:30 +0800

    修改测试策略

--- test-merge.bat
+++ test-merge.bat
@@ -8,15 +8,15 @@ git commit -m"master: change a.xlsx"
 :: Create 'my-file.mrg' on branch 1
 git checkout -b demo-branch-1
 echo "change a.xlsx on demo-branch-1" 
-python.exe test/example.py 7
+python.exe test/example.py 7 -f
 git add test/a.xlsx
 git commit -m"demo-branch-1: change a.xlsx"
 
 :: Create 'my-file.mrg' on branch 2
 git checkout master
-git checkout -b demo-branch-2
+git checkout -b demo-branch-2 
 echo "change a.xlsx on demo-branch-2" 
-python.exe test/example.py 8
+python.exe test/example.py 8 
 git add test/a.xlsx
 git commit -m"demo-branch-2: change a.xlsx"
 
--- test/example.py
+++ test/example.py
@@ -2,11 +2,13 @@ import xlwings as xw
 import sys
 
 s = sys.argv[1]
-
-print('write {} to a.xlsx in cell (1,2)'.format(s))
+change_f = sys.argv[2]
 
 book_a = xw.Book('test/a.xlsx')
 book_a.sheets[0].range((1, 2)).value = s
+if change_f.split():
+    book_a.sheets[0].range(1,3).formula = '=SUM(A1:B1)+{}'.format(s)
+    
 book_a.save()
 keys = xw.apps.keys()
 for key in keys:
