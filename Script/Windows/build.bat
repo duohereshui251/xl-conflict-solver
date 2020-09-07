68f3e04c4fc4850ed36bf59b46dce96be142fec1
hh <10086@qq.com>
2020-09-07 21:18:09 +0800

    修改编译脚本

--- Script/Windows/build.bat
+++ Script/Windows/build.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-pyinstaller --onefile .\src\Diff\diff.py --name=ExlDiff.exe  --icon .\Script\Windows\logo.ico
-pyinstaller --onefile .\src\merge\merge.py --name=ExlMerge.exe  --icon .\Script\Windows\logo.ico
+pyinstaller --onefile .\src\Diff\diff.py --name=ExlDiff-git.exe  --icon .\Script\Windows\logo.ico
+pyinstaller --onefile .\src\merge\merge.py --name=ExlMerge-git.exe  --icon .\Script\Windows\logo.ico
